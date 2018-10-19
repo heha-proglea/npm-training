709030224b3ffee6205fc41390911544c1a4464b
heha-proglea <heha965+proglea@gmail.com>
2018-10-19 00:51:41 +0000

    add app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 'use strict';
+const request = require('request');
+// require関数を使って、requestパッケージを取得。
+request('http://www.google.com', (error, response, body) => {
+    console.log(body);
+});
+// request関数に対して、リクエストのURLとリクエストのレスポンスを受け取った際の無名関数を渡す
